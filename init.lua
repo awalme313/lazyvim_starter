ea44047db4c5fbc1153e1ae8db733930314a54b2
Axel Wålme <axel.walme@google.com>
2026-06-04 22:52:08 +0200

    updated init, keymaps and options and also added some lsp stuff

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+print("Hello, from the init.lua")
 require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>fh", function()
+  Snacks.picker.files({ cwd = "~" })
+end, { desc = "Find files from home" })
+
+vim.keymap.set("n", "<leader>fp", function()
+  Snacks.picker.files({ cwd = "~/projects", hidden = true })
+end, { desc = "Find files from projects" })
